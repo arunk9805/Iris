bcee5e628e31230fec5c7889316e6e81cd5c883d
arunk9805 <arunk9805@gmail.com>
2024-03-11 02:11:23 +0530

    user.controller.js and some bug fix

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,5 +1,8 @@
+import { User } from "../models/user.model.js";
+import { ApiError } from "../utils/ApiErrors.js";
+import { ApiResponse } from "../utils/ApiResponse.js";
 import { asyncHandler } from "../utils/asyncHandler.js";
-
+import {uploadOnCloudinary } from "../utils/couldinary.js"
 
 const registerUser = asyncHandler( async (req, res)=> {
     // steps:
@@ -15,6 +18,55 @@ const registerUser = asyncHandler( async (req, res)=> {
     const {fullName, email, username, password } = req.body
     console.log("email: ", email);
 
+    if(
+        [fullName, email, username, password].some((field) => 
+        field?.trim()  === "")
+    ) {
+        throw new ApiError(400, "All fields are required!")
+    }
+
+    const existedUser = User.findOne({
+        $or: [{username}, {email}]
+    })
+
+    if(existedUser) {
+        throw new ApiError(409, "User with email or username already exists")
+    }
+
+    const avatarLocalPath = req.field?.avatar[0]?.path;
+    const coverImageLocalPath =  req.files?.coverImage[0]?.path;
+
+    if(!avatarLocalPath) {
+        throw new ApiError(400, "Avatar file is required!")
+    }
+
+    const avatar = await uploadOnCloudinary(avatarLocalPath)
+    const coverImage = await uploadOnCloudinary(coverImageLocalPath)
+
+    if(!avatar) {
+        throw new ApiError(400, "Avatar field is required")
+    }
+
+    User.create({
+        fullName,
+        avatar: avatar.url,
+        coverImage: coverImage.url || "",
+        email,
+        password,
+        username:username.toLowerCase()
+    })
+
+    const createdUser = await User.findById(user._id).select(
+        "-password -refreshToken"
+    )
+
+    if(!createdUser) {
+        throw new ApiError(500, "Something went wrong while registring a user!")
+    }
+
+    return res.status(201).json(
+        new ApiResponse(200, createdUser, "User registered successfully")
+    )
 })
 
 
